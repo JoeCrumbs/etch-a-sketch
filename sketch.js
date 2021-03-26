d4014b2dab1cfaf71e9a1a22072fe234d3c0858b
Joe Broesel <broesel.joe@googlemail.com>
2021-03-26 19:12:46 +0100

    final solution

--- sketch.js
+++ sketch.js
@@ -1,90 +1,113 @@
-const CLEAR = document.querySelector('#clear');
-const CONTAINER = document.querySelector('#sketch-container');
-const RADIO_RGB = document.querySelector('#rgb');
-const COLUMN_SLIDER = document.querySelector('#column-number');
-const TEXT_INPUT = document.querySelector('#text-input-slider');
-let useRGB = false;
+(function() {
 
-function createSketchGrid(columnNumber) {
-    let gridNode;
-    // To Do: get grdelements to resize responsively
-    //let mousePressed = false; // use for mousedown solution
-    CONTAINER.setAttribute('style','display:grid; grid-template: repeat(' + columnNumber+ ', 1fr) / repeat('
-            + columnNumber+ ', 1fr)');
-    for (i = 1; i <= columnNumber * columnNumber; i++) {
-        gridNode = document.createElement('div');
-        gridNode.classList.add('sketch-container__div');
-        //if (i % columnNumber === 1) gridNode.style.clear = 'left';
-        /* use for mousedown solution
-        gridNode.addEventListener('mousedown', function() {
-            mousePressed = true;
+    function createSketchGrid(columnNumber) {
+        let gridNode;
+        let gridNodeCounter = 1;
+        let formerElementCounter = null;
+        CONTAINER.setAttribute('style','display:grid; grid-template: repeat(' + columnNumber+ ', 1fr) / repeat('
+                + columnNumber+ ', 1fr)');
+        for (i = 1; i <= columnNumber * columnNumber; i++) {
+            gridNode = document.createElement('div');
+            gridNode.classList.add('sketch-container__div');
+            gridNode.dataset.counter = (gridNodeCounter++).toString();
+
+            //use draw functions on touch / hover with mousedown
+            gridNode.addEventListener('mouseleave', function() {
+                formerElementCounter = setupDrawListener(this, mousePressed, formerElementCounter, false);
+            });
+            gridNode.addEventListener('touchmove', function(touch) {
+                touch.preventDefault();
+                el = document.elementFromPoint(touch.touches[0].clientX, touch.touches[0].clientY);
+                formerElementCounter = setupDrawListener(el, mousePressed, formerElementCounter, true)
+            });
+            CONTAINER.appendChild(gridNode);
+        }
+    }
+
+    function setupDrawListener(el,mousePressed, formerElementCounter, isMobile) {
+        // check if element is in drawing grid
+        if (el && el.dataset && el.dataset.counter && (mousePressed || isMobile)) {
+            curElementCounter = el.dataset.counter;
+        } else return null;
+        if (curElementCounter !== formerElementCounter) { //only draw when moving to new grid (important for touch)
+            // Erase when eraser is toggled
+            if(changeListeners.eraser) {
+                el.style.backgroundColor= '';
+                el.style.filter = '';
+                return curElementCounter;
+            }
+            //use random color if rgb is toggled
+            //colors will bw overwritten as long as shading is not used
+            if(!changeListeners.rgb) {
+                el.style.backgroundColor= COLORPICKER.value;
+            } else if (!changeListeners.shade || (changeListeners.shade && el.style.backgroundColor === '')) {
+                el.style.backgroundColor = setRandomRGBColor() ;
+            }
+            // use shade if shade is toggled
+            if (changeListeners.shade) {
+                el.style.filter = decreaseBrightness(el.style);
+            }
+        }
+        return curElementCounter;
+    }
+
+    function setRandomRGBColor() {
+        return 'rgb(' + getRandomRGBValue() + ',' + getRandomRGBValue() + ',' + getRandomRGBValue();
+    }
+
+    function getRandomRGBValue() {
+        let randomValue = Math.floor(Math.random() * 266);
+        return randomValue;
+    }
+
+    function decreaseBrightness(elStyle) {
+        if (elStyle.filter === '' && elStyle.backgroundColor === '') {
+            return 'brightness(100%)';
+        } else if (elStyle.filter === '') {
+            return 'brightness(90%)';
+        } else {
+            let brightness = +elStyle.filter.substring(elStyle.filter.indexOf('(') + 1, elStyle.filter.indexOf('%')) - 10;
+            return 'brightness(' + brightness + '%)';
+        }
+    };
+
+
+    //setup all EventListeners
+    const CLEAR = document.querySelector('#clear');
+    const CONTAINER = document.querySelector('#sketch-container');
+    const COLUMN_SLIDER = document.querySelector('#column-number');
+    const GRID_TEXT = document.querySelector('#gridsize');
+    const COLORPICKER = document.querySelector('#colorpicker');
+    const HTML = document.querySelector('html');
+    const TOGGLERS = document.querySelectorAll('input[type=checkbox]');
+    let mousePressed = false;
+
+    //setum EventListeners for Toggle Settings
+    let changeListeners = {};
+    TOGGLERS.forEach(function(toggler) {
+        changeListeners[toggler.id] = false;
+        toggler.addEventListener('change', function() {
+            changeListeners[toggler.id] = this.checked;
         });
-        gridNode.addEventListener('mouseup', function() {
-            mousePressed = false;
-        }); */
-        gridNode.addEventListener('mouseleave', function() {
-            //if (mousePressed) {
-                if(!useRGB) {
-                    this.classList.add('sketch-container__div--hovered');
-                } else {
-                    if (this.style.backgroundColor === '') {
-                        this.style.backgroundColor = setRandomRGBColor()
-                    } else {
-                        this.style.filter = decreaseBrightness(this.style.filter);
-                    }
-                }
-            //}
-        });
-        CONTAINER.appendChild(gridNode);
-    }
-}
-/*
-function getNewgridSize(message, messageInvalid) {
-    let columnNumber = +prompt(message);
-    if (columnNumber === null) {
-        return 50;
-    } else if (!(typeof columnNumber === 'number' &&  0 < columnNumber  && columnNumber <= 100)) {
-        return getNewgridSize(messageInvalid, messageInvalid);
-    } else {
-        return columnNumber;
-    }
-}
-*/
+    });
 
-function setRandomRGBColor() {
-    return 'rgb(' + getRandomRGBValue() + ',' + getRandomRGBValue() + ',' + getRandomRGBValue();
-}
+    //setup EventListeners for mousedown solution
+    HTML.addEventListener('mousedown', function() {
+        mousePressed = true;
+    });
+    HTML.addEventListener('mouseup', function() {
+        mousePressed = false;
+    });
 
-function getRandomRGBValue() {
-    let randomValue = Math.floor(Math.random() * 266);
-    return randomValue;
-}
+    CLEAR.addEventListener('click', function() {
+        CONTAINER.innerHTML = '';
+        let columnNumber = COLUMN_SLIDER.value;
+        createSketchGrid(columnNumber);
+    });
 
-function decreaseBrightness(elStyle) {
-    if (elStyle === '') {
-        return 'brightness(90%)';
-    } else {
-        let brightness = +elStyle.substring(elStyle.indexOf('(') + 1, elStyle.indexOf('%')) - 10;
-        return 'brightness(' + brightness + '%)';
-    }
-};
+    COLUMN_SLIDER.addEventListener('change',function() {
+        GRID_TEXT.textContent = this.value + ' x ' + this.value;
+    });
 
-CLEAR.addEventListener('click', function() {
-    CONTAINER.innerHTML = '';
-    let columnNumber = COLUMN_SLIDER.value;
-    createSketchGrid(columnNumber);
-});
-
-RADIO_RGB.addEventListener('change', function() {
-    useRGB = this.checked;
-});
-
-COLUMN_SLIDER.addEventListener('change',function() {
-    TEXT_INPUT.value = this.value;
-});
-
-TEXT_INPUT.addEventListener('change',function() {
-    COLUMN_SLIDER.value = this.value;
-});
-
-createSketchGrid(50);
+    createSketchGrid(50);
+})();
