72817efa39a6209bff458b1d6f7727da639b9d93
Joe Broesel <broesel.joe@googlemail.com>
2021-03-30 21:34:55 +0200

    logic improvement, use more performant solution for darkening

--- sketch.js
+++ sketch.js
@@ -9,6 +9,7 @@
         for (i = 1; i <= columnNumber * columnNumber; i++) {
             gridNode = document.createElement('div');
             gridNode.classList.add('sketch-container__div');
+            gridNode.style.backgroundColor = 'rgb(255,255,255)';
             gridNode.dataset.counter = (gridNodeCounter++).toString();
 
             //use draw functions on touch / hover with mousedown
@@ -30,22 +31,14 @@
             curElementCounter = el.dataset.counter;
         } else return null;
         if (curElementCounter !== formerElementCounter) { //only draw when moving to new grid (important for touch)
-            // Erase when eraser is toggled
-            if(changeListeners.eraser) {
-                el.style.backgroundColor= '';
-                el.style.filter = '';
-                return curElementCounter;
-            }
-            //use random color if rgb is toggled
-            //colors will bw overwritten as long as shading is not used
-            if(!changeListeners.rgb) {
+            if(setting === 'color') {
                 el.style.backgroundColor= COLORPICKER.value;
-            } else if (!changeListeners.shade || (changeListeners.shade && el.style.backgroundColor === '')) {
-                el.style.backgroundColor = setRandomRGBColor() ;
-            }
-            // use shade if shade is toggled
-            if (changeListeners.shade) {
-                el.style.filter = decreaseBrightness(el.style);
+            } else if(setting === 'eraser') { // Erase when eraser is toggled
+                el.style.backgroundColor= 'rgb(255,255,255)';
+            } else if(setting === 'rgb') { //use random color if rgb is toggled
+                el.style.backgroundColor= setRandomRGBColor();
+            } else if (setting === 'shade') { // use shade if shade is toggled
+                el.style.backgroundColor = decreaseBrightness(el.style.backgroundColor);
             }
         }
         return curElementCounter;
@@ -60,16 +53,14 @@
         return randomValue;
     }
 
-    function decreaseBrightness(elStyle) {
-        if (elStyle.filter === '' && elStyle.backgroundColor === '') {
-            return 'brightness(100%)';
-        } else if (elStyle.filter === '') {
-            return 'brightness(90%)';
-        } else {
-            let brightness = +elStyle.filter.substring(elStyle.filter.indexOf('(') + 1, elStyle.filter.indexOf('%')) - 10;
-            return 'brightness(' + brightness + '%)';
-        }
-    };
+    function decreaseBrightness(backgroundColor) {
+        let rgbArr = backgroundColor.slice(4,-1)
+                                    .split(',')
+                                    .map(function(el) {
+                                        return (+el - 25).toString();
+                                    });
+        return 'rgb(' + rgbArr[0] + ',' + rgbArr[1] + ',' + rgbArr[2] + ')';
+    }
 
 
     //setup all EventListeners
@@ -79,15 +70,14 @@
     const GRID_TEXT = document.querySelector('#gridsize');
     const COLORPICKER = document.querySelector('#colorpicker');
     const HTML = document.querySelector('html');
-    const TOGGLERS = document.querySelectorAll('input[type=checkbox]');
+    const TOGGLERS = document.querySelectorAll('input[type=radio]');
     let mousePressed = false;
 
     //setum EventListeners for Toggle Settings
-    let changeListeners = {};
+    let setting = 'color';
     TOGGLERS.forEach(function(toggler) {
-        changeListeners[toggler.id] = false;
         toggler.addEventListener('change', function() {
-            changeListeners[toggler.id] = this.checked;
+            setting = this.id;
         });
     });
 
